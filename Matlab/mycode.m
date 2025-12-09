2356feaf49dbfedf8716677c9f916af3fb3a9837
Maaike Fonteijn <maaike.fonteijn@univ-grenoble-alpes.fr>
2025-12-09 15:00:15 +0100

    added topography plot function

--- Matlab/mycode.m
+++ Matlab/mycode.m
@@ -1,7 +1,25 @@
 % Addition
 
-C = A - B
+C = A + B
 
-% Multiplication
+% Division
 
 F = D / E
+
+% Percentage
+
+P = (G / H) * 100
+
+
+
+% Topography plot function
+function plot_topography(X, height_km)
+    figure;
+    plot(X, height_km*1e3);
+    xlabel('Distance (km)');
+    ylabel('Elevation (m)');
+    title('Topographic Profile');
+    axis ij;
+end
+
+
