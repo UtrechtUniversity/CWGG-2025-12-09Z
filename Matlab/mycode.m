2050cce05362f90e5b291a6f73cdec35272b0c8a
Ylona van Dinther <y.vandinther@uu.nl>
2025-12-09 14:59:45 +0100

    Added square root + combination + visualization

--- Matlab/mycode.m
+++ Matlab/mycode.m
@@ -1,7 +1,26 @@
+% Input parameters
+A = 2;
+B = 7;
+D = [0 10];
+E = [A B];
+
 % Addition
 
-C = A - B
+C = A + B;
 
 % Multiplication
 
-F = D / E
+F = D * E;
+
+% Square root
+
+G = sqrt(C);
+
+% Combine 
+
+H = D * E;
+
+% Visualization
+
+figure(1);
+pcolor(H);
